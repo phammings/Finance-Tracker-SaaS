08b49465da8d3dcbe94e8d04d367631bb3790bb9
Ryan Pham <ryanlepham9@gmail.com>
2024-06-21 22:51:56 -0400

    Implemented GET API and error handling with query providers

---
+++ app/api/[[...route]]/accounts.ts
@@ -0,0 +1,57 @@
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { Hono } from "hono";
+import { eq } from "drizzle-orm";
+import { createId } from "@paralleldrive/cuid2";
+import { zValidator } from "@hono/zod-validator";
+
+import { db } from "@/db/drizzle";
+import { accounts, insertAccountsSchema } from "@/db/schema";
+
+const app = new Hono()
+  .get("/", clerkMiddleware(), async (c) => {
+    const auth = getAuth(c);
+
+    if (!auth?.userId) {
+      return c.json({ error: "unauthorized" }, 401);
+    }
+
+    const data = await db
+      .select({
+        id: accounts.id,
+        name: accounts.name,
+      })
+      .from(accounts)
+      .where(eq(accounts.userId, auth.userId));
+    return c.json({ data });
+  })
+  .post(
+    "/",
+    clerkMiddleware(),
+    zValidator(
+      "json",
+      insertAccountsSchema.pick({
+        name: true,
+      })
+    ),
+    async (c) => {
+      const auth = getAuth(c);
+      const values = c.req.valid("json");
+
+      if (!auth?.userId) {
+        return c.json({ error: "unauthorized" }, 401);
+      }
+
+      const [data] = await db
+        .insert(accounts)
+        .values({
+          id: createId(),
+          userId: auth.userId,
+          ...values,
+        })
+        .returning();
+
+      return c.json({ data });
+    }
+  );
+
+export default app;
