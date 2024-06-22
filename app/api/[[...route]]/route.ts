b9f1ce1fceed1e34b0f3cf42ffc8acdaa4f9cb13
Ryan Pham <ryanlepham9@gmail.com>
2024-06-21 22:31:00 -0400

    Created accounts endpoint

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,16 +1,13 @@
-import { z } from 'zod';
-import { zValidator } from '@hono/zod-validator';
-import { Hono } from 'hono';
-import { handle } from 'hono/vercel';
-import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
 
 import accounts from "./accounts";
 
-export const runtime = 'edge';
+export const runtime = "edge";
 
-const app = new Hono().basePath('/api');
+const app = new Hono().basePath("/api");
 
-app.route("/accounts", accounts);
+const routes = app.route("/accounts", accounts);
 
 export const GET = handle(app);
 export const POST = handle(app);
