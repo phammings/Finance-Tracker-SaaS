7243d624e5d52d5331e681c2f02b1c88ecfb9351
Ryan Pham <ryanlepham9@gmail.com>
2024-06-21 20:14:12 -0400

    Created middleware for protection

---
+++ middleware.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isProtectedRoute = createRouteMatcher([
+    '/',
+  ]);
+
+export default clerkMiddleware((auth, request) => {
+    if (isProtectedRoute(request)) {
+        auth().protect();
+    }
+
+    return NextResponse.next();
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
