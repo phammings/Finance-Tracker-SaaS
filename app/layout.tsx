a3fd324ea1b933e37b9b98597fd135c5f1edd44a
Ryan Pham <ryanlepham9@gmail.com>
2024-06-22 17:08:44 -0400

    Added toast sonner for success and error messages

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 
 import { QueryProvider } from "@/providers/query-provider";
 import { SheetProvider } from "@/providers/sheet-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
         <body className={inter.className}>
           <QueryProvider>
             <SheetProvider />
+            <Toaster />
               {children}
           </QueryProvider>
         </body>
