0dce2fe093e59116e12874639d20eba1f2194759
Ryan Pham <ryanlepham9@gmail.com>
2024-06-21 22:30:07 -0400

    Created accounts endpoint

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,28 +1,16 @@
 import { z } from 'zod';
+import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono';
 import { handle } from 'hono/vercel';
-import { zValidator } from '@hono/zod-validator';
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
+
+import accounts from "./accounts";
 
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api');
 
-app.get(
-  '/hello',
-  clerkMiddleware(),
-  (c) => {
-    const auth = getAuth(c);
-
-    if (!auth?.userId) {
-      return c.json( { error: "Unauthorized" });
-    }
-
-  return c.json({
-    message: 'Hello Next.js!',
-    userId: auth.userId,
-  })
-})
+app.route("/accounts", accounts);
 
 export const GET = handle(app);
 export const POST = handle(app);
