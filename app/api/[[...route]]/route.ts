21126d005eeb06b30a721bd7815e429c5f92f771
Ryan Pham <ryanlepham9@gmail.com>
2024-06-21 20:53:23 -0400

    Sample api

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,15 +1,28 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
+import { z } from 'zod';
+import { Hono } from 'hono';
+import { handle } from 'hono/vercel';
+import { zValidator } from '@hono/zod-validator';
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
 
-export const runtime = 'edge'
+export const runtime = 'edge';
 
-const app = new Hono().basePath('/api')
+const app = new Hono().basePath('/api');
 
-app.get('/hello', (c) => {
+app.get(
+  '/hello',
+  clerkMiddleware(),
+  (c) => {
+    const auth = getAuth(c);
+
+    if (!auth?.userId) {
+      return c.json( { error: "Unauthorized" });
+    }
+
   return c.json({
     message: 'Hello Next.js!',
+    userId: auth.userId,
   })
 })
 
-export const GET = handle(app)
-export const POST = handle(app)
+export const GET = handle(app);
+export const POST = handle(app);
