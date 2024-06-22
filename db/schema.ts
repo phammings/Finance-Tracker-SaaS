6e9ff37c3465eb804bec9d1c441614879aa9ad11
Ryan Pham <ryanlepham9@gmail.com>
2024-06-21 22:15:00 -0400

    Updated schema

--- db/schema.ts
+++ db/schema.ts
@@ -3,7 +3,6 @@ import { createInsertSchema } from "drizzle-zod";
 
 export const accounts = pgTable("accounts", {
   id: text("id").primaryKey(),
-  plaidId: text("plaid_id"),
   name: text("name").notNull(),
   userId: text("user_id").notNull(),
 });
