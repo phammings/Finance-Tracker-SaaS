c6be466ef6f267041f6c6cdac10cc7b02817269d
Ryan Pham <ryanlepham9@gmail.com>
2024-06-23 00:27:29 -0400

    Fixed hydration issue

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -47,7 +47,7 @@ export const Navigation = () => {
   if (isMobile) {
     return (
       <Sheet open={isOpen} onOpenChange={setIsOpen}>
-        <SheetTrigger>
+        <SheetTrigger asChild>
           <Button
             variant="outline"
             size="sm"
