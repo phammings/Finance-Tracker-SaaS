d3e05250963deeec33380f1654a6c1f7bd6ac8fd
Ryan Pham <ryanlepham9@gmail.com>
2024-06-23 20:34:53 -0400

    Added dialog and bulk delete confirmation text

--- components/Data-table.tsx
+++ components/Data-table.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import * as React from "react";
+import { useConfirm } from "@/hooks/use-confirm"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 
@@ -42,6 +43,12 @@ export function DataTable<TData, TValue>({
     onDelete,
     disabled,
 }: DataTableProps<TData, TValue>) {
+    const[ConfirmDialog, confirm] = useConfirm(
+        "Are you sure?",
+        "You are about to perform a bulk delete."
+    );
+
+
     const [sorting, setSorting] = React.useState<SortingState>([]);
     const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
         []
@@ -67,6 +74,7 @@ export function DataTable<TData, TValue>({
 
     return (
         <div>
+            <ConfirmDialog />
             <div className="flex items-center py-4">
                 <Input
                     placeholder={`Filter ${filterKey}...`}
@@ -82,9 +90,13 @@ export function DataTable<TData, TValue>({
                         size="sm"
                         variant="outline"
                         className="ml-auto font-normal text-xs"
-                        onClick={() => {
-                            onDelete(table.getFilteredSelectedRowModel().rows);
-                            table.resetRowSelection();
+                        onClick={async () => {
+                            const ok = await confirm();
+
+                            if (ok) {
+                                onDelete(table.getFilteredSelectedRowModel().rows);
+                                table.resetRowSelection();
+                            }
                         }}
                     >
                         <Trash className="size-4 mr-2" />
