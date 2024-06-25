a0b3e536ddc4d749e79344ba82d86275347b1152
Ryan Pham <ryanlepham9@gmail.com>
2024-06-25 11:11:49 -0400

    Added Summary API route

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -4,6 +4,7 @@ import { handle } from "hono/vercel";
 import accounts from "./accounts";
 import categories from "./categories";
 import transactions from "./transactions";
+import summary from "./summary";
 
 export const runtime = "edge";
 
@@ -12,7 +13,8 @@ const app = new Hono().basePath("/api");
 const routes = app
     .route("/accounts", accounts)
     .route("/categories", categories)
-    .route("/transactions", transactions);
+    .route("/transactions", transactions)
+    .route("/summary", summary);
 
 export const GET = handle(app);
 export const POST = handle(app);
